7b9555a56891fba678c431ad373ab186d375579b
20zakari11-cloud <20zakari11@gmail.com>
2026-07-03 04:18:00 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,44 +1,44 @@
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace 'com.stardo'
-    compileSdk 34
+    namespace = "com.stardo"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.stardo"
-        minSdk 24
-        targetSdk 34
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.stardo"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            minifyEnabled false
+            isMinifyEnabled = false
         }
     }
 
     buildFeatures {
-        compose true
+        compose = true
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion '1.5.14'
+        kotlinCompilerExtensionVersion = "1.5.14"
     }
 
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    implementation "androidx.core:core-ktx:1.13.1"
-    implementation "androidx.activity:activity-compose:1.9.2"
-    implementation "androidx.compose.ui:ui:1.6.8"
-    implementation "androidx.compose.material3:material3:1.2.1"
-    implementation "androidx.compose.ui:ui-tooling-preview:1.6.8"
-    debugImplementation "androidx.compose.ui:ui-tooling:1.6.8"
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation("androidx.compose.ui:ui:1.6.8")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.8")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.6.8")
 }
